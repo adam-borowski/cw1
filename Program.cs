a7f931be24c4b816bf229a18e663d7aa056899fc
Adam Borowski <s18164@pjwstk.edu.pl>
2020-10-27 12:25:32 +0100

    Nowe wymagania

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -8,15 +10,37 @@ namespace cw1
     {
         static async Task Main(string[] args)
         {
+            if (args.Length == 0) {
+                throw new ArgumentNullException();
+            }
+
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(args[0]);
-            if (response.IsSuccessStatusCode) {
-                var html = await response.Content.ReadAsStringAsync();
-                var rx = new Regex("[a-zA-Z0-9\\.\\-_]+@[a-zA-Z0-9\\-_]+\\.[a-zA-Z0-9\\.]+");
-                var matches = rx.Matches(html);
-                foreach (var match in matches) {
-                    System.Console.WriteLine(match);
+            try {
+                
+                var response = await httpClient.GetAsync(args[0]);
+                if (response.IsSuccessStatusCode) {
+                    var html = await response.Content.ReadAsStringAsync();
+                    var rx = new Regex("[a-zA-Z0-9\\.\\-_]+@[a-zA-Z0-9\\-_]+\\.[a-zA-Z0-9\\.]+");
+                    HashSet<String> emails = new HashSet<String>();
+                    var matches = rx.Matches(html);
+
+                    if (matches.Count == 0) {
+                        System.Console.WriteLine("Nie znaleziono adresów email.");
+                    } else {
+                        foreach (var match in matches) {
+                            emails.Add(match.ToString());
+                        }
+                        foreach (String email in emails) {
+                            System.Console.WriteLine(email);
+                        }
+                    }
                 }
+            } catch (InvalidOperationException) {
+                throw new ArgumentException();
+            } catch (HttpRequestException) {
+                System.Console.WriteLine("Błąd w czasie pobierania strony");
+            } finally {
+                httpClient.Dispose();
             }
         }
     }
